7da507a25f6b9ba3540af0a1a12f0140a64cf1f9
sinemorzambak <54941120+sinemorzambak@users.noreply.github.com>
2021-06-25 19:57:30 +0300

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("nameSurname").innerHTML = "Sinem Morzambak";
+    document.getElementById("age").innerHTML = "21";
+    button.style.visibility = "hidden";
+}
+
+const a = document.querySelector('#linkedinUrl');
+a.href = 'https://www.linkedin.com/in/sinem-morzambak/';
+
+const b = document.querySelector('#githubUrl');
+b.href = 'https://github.com/sinemorzambak';
